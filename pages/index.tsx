73da9524b4c0a9569561a4260f4924bc15930464
Divyanshu Tyagi <32699049+dvynshu95@users.noreply.github.com>
2023-07-21 01:59:31 +0530

    Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,35 +11,7 @@ export default function Home() {
       <div className="main">
         <div className="main-container">
           <div id="main-container-inner" className="main-container-inner">
-            <div>
-              {" "}
-              <svg
-                width={300}
-                height={300}
-                data-name="corner-top-left"
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ pointerEvents: "none" }}
-                data-inlinesvg=".inlineSvgFile-4"
-              >
-                <defs>
-                  <linearGradient
-                    id="grad1"
-                    x1="0%"
-                    y1="0%"
-                    x2="100%"
-                    y2="100%"
-                  >
-                    <stop offset={0} className="gradient1" />
-                    <stop offset=".5" className="gradient2" />
-                    <stop offset={1} className="gradient1" />
-                  </linearGradient>
-                </defs>
-                <path
-                  fill="url('#grad1')"
-                  d="M8 0h292l-20 20H25a5 5 0 0 0-5 5v255L0 300V8a8 8 0 0 1 8-8Z"
-                />
-              </svg>{" "}
-            </div>
+         
             <div className="inner-container">
               <header>
                 <div className="logo" id="color-click">
@@ -174,11 +146,11 @@ export default function Home() {
                   </ul>
                 </div>
               </header>
-              <main className="projects-overview">
+              <section className="projects-overview">
                 <div className="main-message">
                   <div className="welcome-message">
                     <div className="nameWrapper">
-                      Hi, My name is
+                      <div>Hi, My name is</div>
                       <h1 className="name highlight">Divyanshu Tyagi</h1>
                     </div>{" "}
                     <span id="introduction">
@@ -286,11 +258,11 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-              </main>
+              </section>
               <footer>
                 © {new Date().getFullYear()} divyanshutyagiofficial.com
               </footer>
-              <div>
+              {/* <div>
                 {" "}
                 <svg
                   width={300}
@@ -318,7 +290,7 @@ export default function Home() {
                     d="M292 300H0l20-20h255a5 5 0 0 0 5-5V20l20-20v292a8 8 0 0 1-8 8Z"
                   ></path>
                 </svg>{" "}
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
